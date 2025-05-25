110516bc3406d6a35099243841a326d888eda926
rosen <rose.night0729@gmail.com>
2025-05-25 14:15:54 +0900

    2025.0525 add: gitignore

---
+++ src/app/Http/Controllers/test2contoroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class test2contoroller extends Controller
+{
+    //
+    public function index(){
+        return view('test2');
+    }
+}
